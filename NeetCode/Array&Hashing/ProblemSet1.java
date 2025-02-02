c0a959724139e17a6e214cf9ec392c174b5cc0aa
Syed Faheem <faheem@astroapp.co>
2025-02-02 13:23:21 +0530

    Add contains duplicate

--- NeetCode/Array&Hashing/ProblemSet1.java
+++ NeetCode/Array&Hashing/ProblemSet1.java
@@ -1,4 +1,8 @@
- class Solution{
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution{
     /*
      * Time complexity: O(n)
      * Space complexity: O(2n)=>O(n)
@@ -33,4 +37,63 @@
 
         return result;
     }
+
+
+    ////////////////////////////////////////////////
+
+/*
+ * Method 1: Brute force
+ * TC O(n^2)
+ * SC O(1)
+ */
+    public boolean hasDuplicateI(int[] nums){
+        for(int i=0;i<nums.length;i++){
+            int num=nums[i];
+            for(int j=i+1;j<nums.length;j++){
+                if(num==nums[j])
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    /*
+     * Method 2: Using sorting
+     * TC: O(n*log(n))
+     * SC: O(1)
+     * Although we will be sorting the array in-place but it's not a good practice to modify
+     * the input
+     */
+
+     public boolean hasDuplicateII(int[] nums){
+      Arrays.sort(nums);
+
+      for(int i=1;i<nums.length;i++){
+        if(nums[i]==nums[i-1])
+            return true;
+      }
+
+      return false;
+    }
+
+
+
+    /*
+     * Method 3: Using a set
+     * Create a set and insert the array and compare the length of the set with the array length
+     * TC: O(n)
+     * SC: O(n)
+     */
+public boolean hasDuplicateIII(int[] nums){
+    Set<Number> set = new HashSet<>();
+
+    for(int i=0;i<nums.length;i++){
+        set.add(nums[i]);
+    }
+
+    return set.size()!=nums.length;
+}
+
  }
---
+++ NeetCode/Array&Hashing/README.md
@@ -0,0 +1,2 @@
+# Brief
+Each problem set will have 3 questions implemented in all possible ways

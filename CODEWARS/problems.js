f5eb57689a49e7b10f59a948ed3d1baf4ab819e3
Rizwan Ul Haq <therizwanulhaq@gmail.com>
2025-02-08 13:23:21 +0530

    numba 2

--- CODEWARS/problems.js
+++ CODEWARS/problems.js
@@ -25,4 +25,33 @@ function fakeBin(x) {
     .join("");
 }
 
-console.log(fakeBin("000555111999"));
+// console.log(fakeBin("000555111999"));
+
+//#2. Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because 1square + 2square + 2square = 9
+
+function squareSum(numbers) {
+  squaredArr = numbers.map((num) => num * num);
+  let sum = 0;
+  for (let i = 0; i < squaredArr.length; i++) {
+    sum += squaredArr[i];
+  }
+  return sum;
+}
+
+// could have just used either for loop or map but i forgot
+
+function squareSumUsingMap(numbers) {
+  return numbers.map((n) => n * n).reduce((a, b) => a + b, 0);
+}
+
+function squareSumsUsingForLoop(numbers) {
+  let sum = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    sum += numbers[i] ** 2;
+  }
+  return sum;
+}
+
+console.log(squareSumsUsingForLoop([1, 2, 2]));

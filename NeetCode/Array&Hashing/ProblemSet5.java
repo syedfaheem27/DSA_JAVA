43bafcb8893b137484de70e56531c182ba117c35
Syed Faheem <faheem@astroapp.co>
2025-02-12 01:25:03 +0530

    Add unique email addresses

--- NeetCode/Array&Hashing/ProblemSet5.java
+++ NeetCode/Array&Hashing/ProblemSet5.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProblemSet5 {
     //Problem 1: Sort colors
 
@@ -57,4 +60,85 @@ public class ProblemSet5 {
         }
     }
 
+
+//     //Problem 2: Isomorphic strings
+
+//     /*
+//      * Method 1: Using a map. Basically, mapping characters in s to the characters in t
+//      * If map contains the character in the map and the value is not equal to the respective
+//      * t character. It means they are not isomorhic
+//      */
+
+//      public boolean isIsomorphic(String s, String t) {
+//      if(s.length()!=t.length())
+//         return false;
+
+//      Map<Character,Character> map = new HashMap<>();
+
+//      for(int i=0;i<s.length();i++){
+//         if(map.containsKey(s.charAt(i))){
+//             char val=map.get(s.charAt(i));
+
+//             if(val!=t.charAt(i))
+//                 return false;
+
+
+//         }else if(map.containsKey(t.charAt(i))){
+//             char val=map.get(t.charAt(i));
+
+//             if(val!=s.charAt(i))
+//                 return false;
+
+
+//         }else {
+//             map.put(s.charAt(i), t.charAt(i));
+//         }
+//      }
+//      return true;
+// }
+
+
+
+//Problem 3: Unique email addresses
+
+/*
+ * Method 1: Brute force
+ * TC: O(N * M) where n is the length of the array and m is the average length of the string
+ * SC: O(N)
+ */
+public int numUniqueEmails(String[] emails) {
+    Map<String,Integer> map=new HashMap<>();
+
+    StringBuilder str;
+
+    for(int i=0;i<emails.length;i++){
+        String s=emails[i];
+        str=new StringBuilder();
+
+        boolean isPlus=false;
+        boolean isAtSymbol=false;
+
+        for(int j=0;j<s.length();j++){
+            if(s.charAt(j)=='@')
+                isAtSymbol=true;
+
+            if(!isAtSymbol && isPlus)
+                continue;
+
+            if(s.charAt(j)=='+' && !isAtSymbol){
+                isPlus=true;
+                continue;
+            }
+
+            if(s.charAt(j)=='.' && !isAtSymbol)
+                continue;
+
+            str.append(s.charAt(j));
+        }
+
+        map.put(str.toString(),1);
+
+    }
+    return map.size();
+}
 }

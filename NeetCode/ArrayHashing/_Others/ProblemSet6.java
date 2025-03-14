256d2e67f9fd9c6462777dc314440d3c44b574c2
Syed Faheem <faheem@theastrotime.com>
2025-03-14 12:52:52 +0530

    Add word pattern

---
+++ NeetCode/ArrayHashing/_Others/ProblemSet6.java
@@ -0,0 +1,53 @@
+package _Others;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProblemSet6 {
+    //TC O(N)
+    //SC O(N) //see if you everything can be done using a single map
+    public boolean wordPatternI(String pattern,String s){
+        String[] arr=s.split(" ");
+
+        if(pattern.length()!=arr.length){
+            return false;
+        }
+
+        Map<Character,String> map=new HashMap<>();
+        Map<String,Character> revMap=new HashMap<>();
+
+
+
+        for(int i=0;i<pattern.length();i++){
+            Character ch=pattern.charAt(i);
+            String rest=arr[i];
+
+            if(map.containsKey(ch)){
+                String val=map.get(ch);
+
+                if(!val.equals(rest)){
+                    return false;
+                }
+            }else{
+                map.put(ch, rest);
+            }
+
+
+            if(revMap.containsKey(rest)){
+                Character val=revMap.get(rest);
+
+                if(!val.equals(ch)){
+                    return false;
+                }
+            }else{
+                revMap.put(rest, ch);
+            }
+
+
+
+        }
+
+
+        return true;
+    }
+}

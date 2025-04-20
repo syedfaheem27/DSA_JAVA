2332d205aeaebe0dc9d12191ff76057069ee3f88
Rizwan Ul Haq <therizwanulhaq@gmail.com>
2025-04-20 19:56:30 +0530

    reverseWords

--- CODEWARS/problems.js
+++ CODEWARS/problems.js
@@ -197,3 +197,16 @@ function seriesSum(n) {
   return sum;
 }
 seriesSum(3);
+
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+function reverseWords(str) {
+  return str
+    .split(" ")
+    .map((word) => word.split("").reverse().join(""))
+    .join(" ");
+}

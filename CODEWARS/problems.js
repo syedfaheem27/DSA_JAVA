22433783611fa8f0f343e1ab293116992887a4a0
Rizwan Ul Haq <therizwanulhaq@gmail.com>
2025-04-27 18:51:25 +0530

    testing 1-2-3

--- CODEWARS/problems.js
+++ CODEWARS/problems.js
@@ -198,7 +198,7 @@ function seriesSum(n) {
 }
 seriesSum(3);
 
-// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+// #9 Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
 
 // Examples
 // "This is an example!" ==> "sihT si na !elpmaxe"
@@ -210,3 +210,14 @@ function reverseWords(str) {
     .map((word) => word.split("").reverse().join(""))
     .join(" ");
 }
+
+// #10  Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+// Examples: (Input --> Output)
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+var number = function (array) {
+  return array.map((arr, index) => `${index + 1}: ${arr}`);
+};
